124f9e43b2f23fa7b49be93f8ef173e1e30c3832
obaidahmed13 <obaidahmed738@gmail.com>
2023-11-21 15:19:44 -0500

    IncomeExpense / TransactionList

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header/>
       <div className='container'>
         <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
       </div>
     </div>
     
